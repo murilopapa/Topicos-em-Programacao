67e8089b911733e41e3a25604e0e8496e3a4a17a
Murilo Martos <murilo.mm@puccampinas.edu.br>
2018-09-17 15:02:33 -0300

    v6.1

--- HTML/EX6/js/facil.js
+++ HTML/EX6/js/facil.js
@@ -1,9 +1,13 @@
-function sorteio(){
-    alert("Primeiro sorteio feito");
-    document.form1.botao1.value = Math.floor((Math.random() * 3));
-    document.form1.botao2.value = Math.floor((Math.random() * 3));
-    document.form1.botao3.value = Math.floor((Math.random() * 3));
-    document.form1.botao4.value = Math.floor((Math.random() * 3));
+function sorteio(database){
+
+    var sorteado = Math.floor((Math.random() * 5 ));
+    document.form1.botao1.value = database[Math.floor((Math.random() * 5 ))].nome;
+    document.form1.botao2.value = database[Math.floor((Math.random() * 5 ))].nome;
+    document.form1.botao3.value = database[Math.floor((Math.random() * 5 ))].nome;
+    document.form1.botao4.value = database[Math.floor((Math.random() * 5 ))].nome;
+    alert(database[sorteado].nome);
+    document.getElementById("imagem").innerHTML = "<img width="+"10%"+" height="+"10%"+" src="+ database[sorteado].src +">";
+
 }
 function submissao(num_botao){
     document.form1.botao1.value = "teste";
